e448db421114252bc1455982325d8609f5e731e2
Kyle LePoidevin-Gonzales <lepoidev@ualberta.ca>
2021-04-07 21:06:20 -0700

    Move IRGenerator implementation to source file

--- include/IR/IRGenerator.h
+++ include/IR/IRGenerator.h
@@ -1,12 +1,6 @@
 #pragma once
 
-#include <filesystem>
-#include <fstream>
-
 #include "KLangCommon.h"
-#include "AST/Nodes/Node.h"
-#include "IR/Context.h"
-#include "AST/TypeResolver.h"
 
 namespace IR
 {
@@ -15,34 +9,12 @@ namespace IR
 #pragma region Constructors / Destructors
   public:
     IRGenerator() = delete;
-    IRGenerator( std::string const& outputFile ) : m_outputFile { outputFile }
-    {
-      std::filesystem::path path { m_outputFile };
-      m_moduleName = path.stem();
-    }
+    IRGenerator( std::string const& outputFile );
 #pragma endregion
 
 #pragma region IR Generation
   public:
-    void GenerateIR( ASTNodePtr const& root )
-    {
-      AST::TypeResolver typeResolver {};
-      typeResolver.ResolveTypes( root );
-
-      SymbolTable symbolTable;
-      llvm::LLVMContext globalLLVMContext {};
-      llvm::IRBuilder<> irBuilder { globalLLVMContext };
-      llvm::Module mod { m_moduleName, globalLLVMContext };
-      Context ctx { globalLLVMContext, irBuilder, mod, symbolTable };
-
-      root->GenerateIR( ctx );
-
-      std::ofstream out { m_outputFile };
-      llvm::raw_os_ostream ros { out };
-      llvm::raw_os_ostream llErr { std::cerr };
-      llvm::verifyModule( mod, &llErr );
-      mod.print( ros, nullptr );
-    }
+    void GenerateIR( ASTNodePtr const& root );
 #pragma endregion
 
   private:
--- source/IR/IRGenerator.cpp
+++ source/IR/IRGenerator.cpp
@@ -1,1 +1,37 @@
+#include <filesystem>
+#include <fstream>
+
 #include "IR/IRGenerator.h"
+#include "AST/Nodes/Node.h"
+#include "IR/Context.h"
+#include "AST/TypeResolver.h"
+
+namespace IR
+{
+  IRGenerator::IRGenerator( std::string const& outputFile ) :
+    m_outputFile { outputFile }
+  {
+    std::filesystem::path path { m_outputFile };
+    m_moduleName = path.stem();
+  }
+
+  void IRGenerator::GenerateIR( ASTNodePtr const& root )
+  {
+    AST::TypeResolver typeResolver {};
+    typeResolver.ResolveTypes( root );
+
+    SymbolTable symbolTable;
+    llvm::LLVMContext globalLLVMContext {};
+    llvm::IRBuilder<> irBuilder { globalLLVMContext };
+    llvm::Module mod { m_moduleName, globalLLVMContext };
+    Context ctx { globalLLVMContext, irBuilder, mod, symbolTable };
+
+    root->GenerateIR( ctx );
+
+    std::ofstream out { m_outputFile };
+    llvm::raw_os_ostream ros { out };
+    llvm::raw_os_ostream llErr { std::cerr };
+    llvm::verifyModule( mod, &llErr );
+    mod.print( ros, nullptr );
+  }
+}

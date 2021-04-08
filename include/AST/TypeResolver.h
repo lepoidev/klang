1c5ae8c30b3f40e4db6d26cd8f7ddcff6f251c93
Kyle LePoidevin-Gonzales <lepoidev@ualberta.ca>
2021-04-07 20:59:05 -0700

    Move TypeResolver implementation to source file

--- include/AST/TypeResolver.h
+++ include/AST/TypeResolver.h
@@ -14,65 +14,23 @@ namespace AST
 
 #pragma region Type Resolving
   public:
-    ASTTypePtr const ResolveTypes( ASTNodePtr const& root )
-    {
-      auto const result { root->Accept( *this ) };
-      if( result.has_value() )
-      {
-        return std::any_cast<ASTTypePtr>( result );
-      }
-      return {};
-    }
+    ASTTypePtr const ResolveTypes( ASTNodePtr const& root );
 #pragma endregion
 
 #pragma region Helpers
   private:
     using ScopedVisitChildrenCB = std::function<void()>;
     std::any
-    VisitChildrenWithScope( Node& node, ScopedVisitChildrenCB const& cb = {} )
-    {
-      m_symbolTable.PushNewScope();
-      auto const ret { VisitChildren( node ) };
-      if( cb )
-      {
-        cb();
-      }
-      m_symbolTable.PopScope();
-
-      return ret;
-    }
+    VisitChildrenWithScope( Node& node, ScopedVisitChildrenCB const& cb = {} );
 #pragma endregion
 
 #pragma region Overrides
   private:
-    std::any Visit( IdentifierNode& node ) override
-    {
-      if( auto const symbol {
-            m_symbolTable.ResolveSymbol( node.GetIdentifier() ) };
-          symbol )
-      {
-        node.SetType( symbol->get().GetType() );
-      }
-      return {};
-    }
-    std::any Visit( DeclNode& node ) override
-    {
-      VisitChildren( node );
-      m_symbolTable.AddSymbol( node.GetIdentifier(), {}, node.GetDeclType() );
-      return {};
-    }
-    std::any Visit( BlockNode& node ) override
-    {
-      return VisitChildrenWithScope( node );
-    }
-    std::any Visit( FileNode& node ) override
-    {
-      return VisitChildrenWithScope( node );
-    }
-    std::any Visit( FunctionNode& node ) override
-    {
-      return VisitChildrenWithScope( node );
-    }
+    std::any Visit( IdentifierNode& node ) override;
+    std::any Visit( DeclNode& node ) override;
+    std::any Visit( BlockNode& node ) override;
+    std::any Visit( FileNode& node ) override;
+    std::any Visit( FunctionNode& node ) override;
 #pragma endregion
 
   private:
--- source/AST/TypeResolver.cpp
+++ source/AST/TypeResolver.cpp
@@ -1,1 +1,56 @@
 #include "AST/TypeResolver.h"
+
+namespace AST
+{
+  ASTTypePtr const TypeResolver::ResolveTypes( ASTNodePtr const& root )
+  {
+    auto const result { root->Accept( *this ) };
+    if( result.has_value() )
+    {
+      return std::any_cast<ASTTypePtr>( result );
+    }
+    return {};
+  }
+  std::any
+  TypeResolver::VisitChildrenWithScope( Node& node,
+                                        ScopedVisitChildrenCB const& cb )
+  {
+    m_symbolTable.PushNewScope();
+    auto const ret { VisitChildren( node ) };
+    if( cb )
+    {
+      cb();
+    }
+    m_symbolTable.PopScope();
+
+    return ret;
+  }
+  std::any TypeResolver::Visit( IdentifierNode& node )
+  {
+    if( auto const symbol {
+          m_symbolTable.ResolveSymbol( node.GetIdentifier() ) };
+        symbol )
+    {
+      node.SetType( symbol->get().GetType() );
+    }
+    return {};
+  }
+  std::any TypeResolver::Visit( DeclNode& node )
+  {
+    VisitChildren( node );
+    m_symbolTable.AddSymbol( node.GetIdentifier(), {}, node.GetDeclType() );
+    return {};
+  }
+  std::any TypeResolver::Visit( BlockNode& node )
+  {
+    return VisitChildrenWithScope( node );
+  }
+  std::any TypeResolver::Visit( FileNode& node )
+  {
+    return VisitChildrenWithScope( node );
+  }
+  std::any TypeResolver::Visit( FunctionNode& node )
+  {
+    return VisitChildrenWithScope( node );
+  }
+}

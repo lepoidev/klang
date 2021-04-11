4e7df110a89566c4d9110b824e6b89817beeb72d
Kyle LePoidevin-Gonzales <lepoidev@ualberta.ca>
2021-04-11 10:07:34 -0700

    Move ASTGenerator implementation to source file

--- include/AST/ASTGenerator.h
+++ include/AST/ASTGenerator.h
@@ -1,225 +1,65 @@
 #pragma once
 
 #include <KBaseVisitor.h>
-#include "AST/ASTNodes.h"
-#include "AST/ASTTypes.h"
 
 namespace AST
 {
   class ASTGenerator : public KBaseVisitor
   {
-    antlrcpp::Any visitFile( KParser::FileContext* ctx ) override
-    {
-      std::vector<ASTNodePtr> statements {};
-      for( auto& functionDeclare : ctx->functionDeclare() )
-      {
-        statements.push_back(
-          static_cast<ASTNodePtr>( visit( functionDeclare ) ) );
-      }
-      return CreateGenericNode<FileNode>( statements );
-    }
+    antlrcpp::Any visitFile( KParser::FileContext* ctx ) override;
 
     antlrcpp::Any
-    visitFunctionDeclare( KParser::FunctionDeclareContext* ctx ) override
-    {
-      auto const& functionIdentifier { ctx->functionIdentifier() };
-      std::string const functionName {
-        functionIdentifier->functionName->getText()
-      };
-
-      auto const hasReturnType { functionIdentifier->type() != nullptr };
-      ASTTypePtr const& returnType { hasReturnType ?
-                                       static_cast<ASTTypePtr>(
-                                         visit( functionIdentifier->type() ) ) :
-                                       nullptr };
-
-      std::vector<ASTTypePtr> paramTypes {};
-      auto const& funcBody { static_cast<ASTNodePtr>(
-        visit( ctx->statement() ) ) };
-      return CreateGenericNode<FunctionNode>(
-        functionName, returnType, paramTypes, funcBody );
-    }
+    visitFunctionDeclare( KParser::FunctionDeclareContext* ctx ) override;
 
 #pragma region Literals
     antlrcpp::Any
-    visitIntegerLiteral( KParser::IntegerLiteralContext* ctx ) override
-    {
-      std::string const text { ctx->getText() };
-      auto const val { std::stoi( text ) };
-      return CreateGenericNode<IntegerLiteralNode>( val );
-    }
+    visitIntegerLiteral( KParser::IntegerLiteralContext* ctx ) override;
 
     antlrcpp::Any
-    visitBooleanLiteral( KParser::BooleanLiteralContext* ctx ) override
-    {
-      std::string const text { ctx->getText() };
-      auto const val { text == "true" };
-      return CreateGenericNode<BooleanLiteralNode>( val );
-    }
+    visitBooleanLiteral( KParser::BooleanLiteralContext* ctx ) override;
 #pragma endregion
 
 #pragma region Statements
     antlrcpp::Any visitFunctionCallStatement(
-      KParser::FunctionCallStatementContext* ctx ) override
-    {
-      return visit( ctx->functionCall() );
-    }
+      KParser::FunctionCallStatementContext* ctx ) override;
 
     antlrcpp::Any
-    visitReturnStatement( KParser::ReturnStatementContext* ctx ) override
-    {
-      auto const hasExpr { ctx->expr() != nullptr };
-      ASTNodePtr const expr {
-        hasExpr ? static_cast<ASTNodePtr>( visit( ctx->expr() ) ) : nullptr
-      };
-      return CreateGenericNode<ReturnNode>( expr );
-    }
+    visitReturnStatement( KParser::ReturnStatementContext* ctx ) override;
 
     antlrcpp::Any
-    visitBlockStatement( KParser::BlockStatementContext* ctx ) override
-    {
-      std::vector<ASTNodePtr> statements {};
-      for( auto& statement : ctx->statement() )
-      {
-        statements.push_back( static_cast<ASTNodePtr>( visit( statement ) ) );
-      }
-      return CreateGenericNode<BlockNode>( statements );
-    }
+    visitBlockStatement( KParser::BlockStatementContext* ctx ) override;
 
     antlrcpp::Any visitConditionalStatement(
-      KParser::ConditionalStatementContext* ctx ) override
-    {
-      auto const condExpr { static_cast<ASTNodePtr>(
-        visit( ctx->condition()->expr() ) ) };
-      auto const thenStatement { static_cast<ASTNodePtr>(
-        visit( ctx->statement().at( 0 ) ) ) };
-      ASTNodePtr const elseStatement { ctx->statement().size() > 1 ?
-                                         static_cast<ASTNodePtr>(
-                                           visit( ctx->statement().at( 1 ) ) ) :
-                                         nullptr };
-      return CreateGenericNode<CondNode>(
-        condExpr, thenStatement, elseStatement );
-    }
+      KParser::ConditionalStatementContext* ctx ) override;
 #pragma endregion
 
-    antlrcpp::Any visitBooleanExpr( KParser::BooleanExprContext* ctx ) override
-    {
-      auto const left { static_cast<ASTNodePtr>( visit( ctx->left ) ) };
-      auto const right { static_cast<ASTNodePtr>( visit( ctx->right ) ) };
-      if( auto const& eqOp { ctx->equalityOperation() }; eqOp != nullptr )
-      {
-        if( eqOp->IsEqual() != nullptr )
-        {
-          return CreateGenericNode<EQNode>( left, right );
-        }
-        else if( eqOp->NotIsEqual() != nullptr )
-        {
-          return CreateGenericNode<NEQNode>( left, right );
-        }
-      }
-      else if( auto const& cmpOp { ctx->comparisonOperation() };
-               cmpOp != nullptr )
-      {
-        if( cmpOp->CloseAngle() != nullptr )
-        {
-          return CreateGenericNode<LTNode>( left, right );
-        }
-        else if( cmpOp->LessEqual() != nullptr )
-        {
-          return CreateGenericNode<LTENode>( left, right );
-        }
-        else if( cmpOp->OpenAngle() != nullptr )
-        {
-          return CreateGenericNode<GTNode>( left, right );
-        }
-        else if( cmpOp->GreaterEqual() != nullptr )
-        {
-          return CreateGenericNode<GTENode>( left, right );
-        }
-      }
-      return visitChildren( ctx );
-    }
+    antlrcpp::Any visitBooleanExpr( KParser::BooleanExprContext* ctx ) override;
 
 #pragma region Built In
-    antlrcpp::Any visitAssertExpr( KParser::AssertExprContext* ctx ) override
-    {
-      auto const exprNode { static_cast<ASTNodePtr>( visit( ctx->expr() ) ) };
-      auto const expr { ctx->expr()->getText() };
-      auto const line { ctx->getStart()->getLine() };
-      return CreateGenericNode<AssertNode>( expr, line, exprNode );
-    }
+    antlrcpp::Any visitAssertExpr( KParser::AssertExprContext* ctx ) override;
 #pragma endregion
 
     antlrcpp::Any
-    visitIdentifierExpr( KParser::IdentifierExprContext* ctx ) override
-    {
-      std::string const identifier { ctx->Identifier()->getText() };
-      return CreateGenericNode<IdentifierNode>( identifier );
-    }
+    visitIdentifierExpr( KParser::IdentifierExprContext* ctx ) override;
 
 #pragma region Declaration
-    antlrcpp::Any visitAutoDecl( KParser::AutoDeclContext* ctx ) override
-    {
-      std::string const identifier { ctx->Identifier()->getText() };
-      auto const isMutable { ctx->Mut() != nullptr };
-      auto const& exprNode { static_cast<ASTNodePtr>( visit( ctx->expr() ) ) };
-      auto declType { exprNode->GetType()->Clone() };
-      declType->SetMut( isMutable );
-      return CreateGenericNode<DeclNode>( identifier, declType, exprNode );
-    }
+    antlrcpp::Any visitAutoDecl( KParser::AutoDeclContext* ctx ) override;
 
     antlrcpp::Any
-    visitEmptyInitDecl( KParser::EmptyInitDeclContext* ctx ) override
-    {
-      std::string const identifier { ctx->Identifier()->getText() };
-      auto const declType { static_cast<ASTTypePtr>( visit( ctx->type() ) ) };
-      return CreateGenericNode<DeclNode>( identifier, declType, ASTNodePtr {} );
+    visitEmptyInitDecl( KParser::EmptyInitDeclContext* ctx ) override;
 
-      return {};
-    }
-
-    antlrcpp::Any visitInitDecl( KParser::InitDeclContext* ctx ) override
-    {
-      std::string const identifier { ctx->Identifier()->getText() };
-      auto const declType { static_cast<ASTTypePtr>( visit( ctx->type() ) ) };
-      auto const& exprNode { static_cast<ASTNodePtr>( visit( ctx->expr() ) ) };
-      return CreateGenericNode<DeclNode>( identifier, declType, exprNode );
-
-      return {};
-    }
+    antlrcpp::Any visitInitDecl( KParser::InitDeclContext* ctx ) override;
 #pragma endregion
 
 #pragma region Assignment
-    antlrcpp::Any visitBasicAssign( KParser::BasicAssignContext* ctx ) override
-    {
-      std::string const identifier { ctx->Identifier()->getText() };
-      auto const& exprNode { static_cast<ASTNodePtr>( visit( ctx->expr() ) ) };
-      return CreateGenericNode<AssignNode>( identifier, exprNode );
-    }
+    antlrcpp::Any visitBasicAssign( KParser::BasicAssignContext* ctx ) override;
 #pragma endregion
 
 #pragma region Types
     antlrcpp::Any
-    visitPrimitiveType( KParser::PrimitiveTypeContext* ctx ) override
-    {
-      if( ctx->Integer() )
-      {
-        return CreateGenericType<IntegerType>();
-      }
-      else if( ctx->Bool() )
-      {
-        return CreateGenericType<BooleanType>();
-      }
-      return {};
-    }
+    visitPrimitiveType( KParser::PrimitiveTypeContext* ctx ) override;
 
-    antlrcpp::Any visitType( KParser::TypeContext* ctx ) override
-    {
-      auto astType { static_cast<ASTTypePtr>( visit( ctx->rawType() ) ) };
-      auto const isMutable { ctx->Mut() != nullptr };
-      astType->SetMut( isMutable );
-      return astType;
-    }
+    antlrcpp::Any visitType( KParser::TypeContext* ctx ) override;
 #pragma endregion
   };
 }
--- source/AST/ASTGenerator.cpp
+++ source/AST/ASTGenerator.cpp
@@ -1,1 +1,223 @@
 #include "AST/ASTGenerator.h"
+
+#include "AST/ASTNodes.h"
+#include "AST/ASTTypes.h"
+
+namespace AST
+{
+  antlrcpp::Any ASTGenerator::visitFile( KParser::FileContext* ctx )
+  {
+    std::vector<ASTNodePtr> statements {};
+    for( auto& functionDeclare : ctx->functionDeclare() )
+    {
+      statements.push_back(
+        static_cast<ASTNodePtr>( visit( functionDeclare ) ) );
+    }
+    return CreateGenericNode<FileNode>( statements );
+  }
+
+  antlrcpp::Any
+  ASTGenerator::visitFunctionDeclare( KParser::FunctionDeclareContext* ctx )
+  {
+    auto const& functionIdentifier { ctx->functionIdentifier() };
+    std::string const functionName {
+      functionIdentifier->functionName->getText()
+    };
+
+    auto const hasReturnType { functionIdentifier->type() != nullptr };
+    ASTTypePtr const& returnType { hasReturnType ?
+                                     static_cast<ASTTypePtr>(
+                                       visit( functionIdentifier->type() ) ) :
+                                     nullptr };
+
+    std::vector<ASTTypePtr> paramTypes {};
+    auto const& funcBody { static_cast<ASTNodePtr>(
+      visit( ctx->statement() ) ) };
+    return CreateGenericNode<FunctionNode>(
+      functionName, returnType, paramTypes, funcBody );
+  }
+
+#pragma region Literals
+  antlrcpp::Any
+  ASTGenerator::visitIntegerLiteral( KParser::IntegerLiteralContext* ctx )
+  {
+    std::string const text { ctx->getText() };
+    auto const val { std::stoi( text ) };
+    return CreateGenericNode<IntegerLiteralNode>( val );
+  }
+
+  antlrcpp::Any
+  ASTGenerator::visitBooleanLiteral( KParser::BooleanLiteralContext* ctx )
+  {
+    std::string const text { ctx->getText() };
+    auto const val { text == "true" };
+    return CreateGenericNode<BooleanLiteralNode>( val );
+  }
+#pragma endregion
+
+#pragma region Statements
+  antlrcpp::Any ASTGenerator::visitFunctionCallStatement(
+    KParser::FunctionCallStatementContext* ctx )
+  {
+    return visit( ctx->functionCall() );
+  }
+
+  antlrcpp::Any
+  ASTGenerator::visitReturnStatement( KParser::ReturnStatementContext* ctx )
+  {
+    auto const hasExpr { ctx->expr() != nullptr };
+    ASTNodePtr const expr { hasExpr ?
+                              static_cast<ASTNodePtr>( visit( ctx->expr() ) ) :
+                              nullptr };
+    return CreateGenericNode<ReturnNode>( expr );
+  }
+
+  antlrcpp::Any
+  ASTGenerator::visitBlockStatement( KParser::BlockStatementContext* ctx )
+  {
+    std::vector<ASTNodePtr> statements {};
+    for( auto& statement : ctx->statement() )
+    {
+      statements.push_back( static_cast<ASTNodePtr>( visit( statement ) ) );
+    }
+    return CreateGenericNode<BlockNode>( statements );
+  }
+
+  antlrcpp::Any ASTGenerator::visitConditionalStatement(
+    KParser::ConditionalStatementContext* ctx )
+  {
+    auto const condExpr { static_cast<ASTNodePtr>(
+      visit( ctx->condition()->expr() ) ) };
+    auto const thenStatement { static_cast<ASTNodePtr>(
+      visit( ctx->statement().at( 0 ) ) ) };
+    ASTNodePtr const elseStatement { ctx->statement().size() > 1 ?
+                                       static_cast<ASTNodePtr>(
+                                         visit( ctx->statement().at( 1 ) ) ) :
+                                       nullptr };
+    return CreateGenericNode<CondNode>(
+      condExpr, thenStatement, elseStatement );
+  }
+#pragma endregion
+
+  antlrcpp::Any
+  ASTGenerator::visitBooleanExpr( KParser::BooleanExprContext* ctx )
+  {
+    auto const left { static_cast<ASTNodePtr>( visit( ctx->left ) ) };
+    auto const right { static_cast<ASTNodePtr>( visit( ctx->right ) ) };
+    if( auto const& eqOp { ctx->equalityOperation() }; eqOp != nullptr )
+    {
+      if( eqOp->IsEqual() != nullptr )
+      {
+        return CreateGenericNode<EQNode>( left, right );
+      }
+      else if( eqOp->NotIsEqual() != nullptr )
+      {
+        return CreateGenericNode<NEQNode>( left, right );
+      }
+    }
+    else if( auto const& cmpOp { ctx->comparisonOperation() };
+             cmpOp != nullptr )
+    {
+      if( cmpOp->CloseAngle() != nullptr )
+      {
+        return CreateGenericNode<LTNode>( left, right );
+      }
+      else if( cmpOp->LessEqual() != nullptr )
+      {
+        return CreateGenericNode<LTENode>( left, right );
+      }
+      else if( cmpOp->OpenAngle() != nullptr )
+      {
+        return CreateGenericNode<GTNode>( left, right );
+      }
+      else if( cmpOp->GreaterEqual() != nullptr )
+      {
+        return CreateGenericNode<GTENode>( left, right );
+      }
+    }
+    return visitChildren( ctx );
+  }
+
+#pragma region Built In
+  antlrcpp::Any ASTGenerator::visitAssertExpr( KParser::AssertExprContext* ctx )
+  {
+    auto const exprNode { static_cast<ASTNodePtr>( visit( ctx->expr() ) ) };
+    auto const expr { ctx->expr()->getText() };
+    auto const line { ctx->getStart()->getLine() };
+    return CreateGenericNode<AssertNode>( expr, line, exprNode );
+  }
+#pragma endregion
+
+  antlrcpp::Any
+  ASTGenerator::visitIdentifierExpr( KParser::IdentifierExprContext* ctx )
+  {
+    std::string const identifier { ctx->Identifier()->getText() };
+    return CreateGenericNode<IdentifierNode>( identifier );
+  }
+
+#pragma region Declaration
+  antlrcpp::Any ASTGenerator::visitAutoDecl( KParser::AutoDeclContext* ctx )
+  {
+    std::string const identifier { ctx->Identifier()->getText() };
+    auto const isMutable { ctx->Mut() != nullptr };
+    auto const& exprNode { static_cast<ASTNodePtr>( visit( ctx->expr() ) ) };
+    auto declType { exprNode->GetType()->Clone() };
+    declType->SetMut( isMutable );
+    return CreateGenericNode<DeclNode>( identifier, declType, exprNode );
+  }
+
+  antlrcpp::Any
+  ASTGenerator::visitEmptyInitDecl( KParser::EmptyInitDeclContext* ctx )
+  {
+    std::string const identifier { ctx->Identifier()->getText() };
+    auto const declType { static_cast<ASTTypePtr>( visit( ctx->type() ) ) };
+    return CreateGenericNode<DeclNode>( identifier, declType, ASTNodePtr {} );
+
+    return {};
+  }
+
+  antlrcpp::Any ASTGenerator::visitInitDecl( KParser::InitDeclContext* ctx )
+  {
+    std::string const identifier { ctx->Identifier()->getText() };
+    auto const declType { static_cast<ASTTypePtr>( visit( ctx->type() ) ) };
+    auto const& exprNode { static_cast<ASTNodePtr>( visit( ctx->expr() ) ) };
+    return CreateGenericNode<DeclNode>( identifier, declType, exprNode );
+
+    return {};
+  }
+#pragma endregion
+
+#pragma region Assignment
+  antlrcpp::Any
+  ASTGenerator::visitBasicAssign( KParser::BasicAssignContext* ctx )
+  {
+    std::string const identifier { ctx->Identifier()->getText() };
+    auto const& exprNode { static_cast<ASTNodePtr>( visit( ctx->expr() ) ) };
+    return CreateGenericNode<AssignNode>( identifier, exprNode );
+  }
+#pragma endregion
+
+#pragma region Types
+  antlrcpp::Any
+  ASTGenerator::visitPrimitiveType( KParser::PrimitiveTypeContext* ctx )
+  {
+    if( ctx->Integer() )
+    {
+      return CreateGenericType<IntegerType>();
+    }
+    else if( ctx->Bool() )
+    {
+      return CreateGenericType<BooleanType>();
+    }
+    return {};
+  }
+
+  antlrcpp::Any ASTGenerator::visitType( KParser::TypeContext* ctx )
+  {
+    auto astType { static_cast<ASTTypePtr>( visit( ctx->rawType() ) ) };
+    auto const isMutable { ctx->Mut() != nullptr };
+    astType->SetMut( isMutable );
+    return astType;
+  }
+#pragma endregion
+}
